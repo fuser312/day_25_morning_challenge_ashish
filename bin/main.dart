aa128bf6593ca4d6c538d85000288bfee01a6269
Ashish <ashishteddy2@gmail.com>
2019-11-11 14:23:09 +0530

    black check issue

--- bin/main.dart
+++ bin/main.dart
@@ -45,24 +45,23 @@ bool validInputs(List<List<int>> board) {
 
 bool checkRow(List<List<int>> board) {
   int count = 0;
-  for (int i = 0; i < 3; i + 3) {
-    if (checkList(board, i)) {
-      count++;
-    }
+  for (List row in board) {
+    if (row
+        .toSet()
+        .length == 9) count++;
   }
   return count == 9;
-  }
+}
 
 bool checkCol(List<List<int>> board) {
   List transposed = transposeMatrix(board);
   int count = 0;
-  for (int i = 0; i < 3; i + 3) {
-    if (checkList(transposed, i)) {
-      count++;
-    }
+  for (List col in transposed) {
+    if (col
+        .toSet()
+        .length == 9) count++;
   }
   return count == 9;
-
 }
 
 bool checkBlock(List<List<int>> board, int row, int col) {
@@ -70,31 +69,33 @@ bool checkBlock(List<List<int>> board, int row, int col) {
   List<int> st = [];
   for (int i = 0; i < 3; i + 3) {
     for (int j = 0; j < 3; j + 3) {
-        int curr = board[i + row][j + col];
-        if (st.contains(curr)) {
-          return false;
-        } else {
-          st.add(curr);
-          print(st);
-        }
-
+      int curr = board[i + row ][j + col ];
+      if (st.contains(curr)) {
+        return false;
+      } else {
+        st.add(curr);
       }
+    }
   }
   return true;
 }
 
-bool isValid(List<List<int>> board, int row, int col) {
-  return checkRow(board) &&
-      checkCol(board);
+bool sudokuValidator(List<List<int>> board) {
+  return checkRow(board) && checkCol(board) && isValidator(board);
 }
 
-bool sudokuValidator(List<List<int>> board) {
+bool isValidator(List<List<int>> board) {
   int n = 9;
-  for (int row = 0; row < n; row++) {
-    for (int col = 0; col < n; col++) {
-      return (isValid(board, row, col) && checkBlock(board, row, col));
+  int count = 0;
+  for (int row = 0; row < 9; row++) {
+    for (int col = 0; col < 9; col++) {
+      if (row % 3 == 0 && col % 3 == 0) {
+        if (checkBlock(board, row, col)) count++;
       }
     }
+  }
+  print('Block count $count');
+  return (count == 9);
 }
 
 // Challenge 3
@@ -137,20 +138,17 @@ int comparator(int a, int b) {
   }
 }
 
-
 main() {
-
-print (sudokuValidator([
-  [ 1, 5, 2, 4, 8, 9, 3, 7, 6 ],
-  [ 7, 3, 9, 2, 5, 6, 8, 4, 1 ],
-  [ 4, 6, 8, 3, 7, 1, 2, 9, 5 ],
-  [ 3, 8, 7, 1, 2, 4, 6, 5, 9 ],
-  [ 5, 9, 1, 7, 6, 3, 4, 2, 8 ],
-  [ 2, 4, 6, 8, 9, 5, 7, 1, 3 ],
-  [ 9, 1, 4, 6, 3, 7, 5, 8, 2 ],
-  [ 6, 2, 5, 9, 4, 8, 1, 3, 7 ],
-  [ 8, 7, 3, 5, 1, 2, 9, 6, 4 ]
-]));
-print(factorSort([9, 7, 13, 12]));
-
+  print(sudokuValidator([
+    [1, 5, 2, 4, 8, 9, 3, 7, 6],
+    [7, 3, 9, 2, 5, 6, 8, 4, 1],
+    [4, 6, 8, 3, 7, 1, 2, 9, 5],
+    [3, 8, 7, 1, 2, 4, 6, 5, 9],
+    [5, 9, 1, 7, 6, 3, 4, 2, 8],
+    [2, 4, 6, 8, 9, 5, 7, 1, 3],
+    [9, 1, 4, 6, 3, 7, 5, 8, 2],
+    [6, 2, 5, 9, 4, 8, 1, 3, 7],
+    [8, 7, 3, 5, 1, 2, 9, 6, 4]
+  ]));
+  print(factorSort([9, 7, 13, 12]));
 }
